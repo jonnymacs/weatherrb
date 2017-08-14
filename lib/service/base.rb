f93dac5fe4faf35e04e9b88a541f8bd0f4f57ae2
jonnymac <jmcallis@gmail.com>
2017-08-14 12:18:47 -0700

    slight change to error handling. the Service::Error module is supposed to be generic. It shouldn't have any expectation about how to parse an error message from an api response.
    For the purpose of this exercise I'm doing it here, but I
    should really implement api specific error parsing

--- lib/service/base.rb
+++ lib/service/base.rb
@@ -8,7 +8,7 @@ module Service
         return response.code, response.parsed_response if response.code == 200
         return response.code, nil if response.code == 404
 
-        raise ::Service::Error::NetworkError.new(action, self.name.split('::').first, params, response)
+        raise ::Service::Error::NetworkError.new(action, self.name.split('::').first, params, response.parsed_response)
       rescue HTTParty::ResponseError => exception
         raise ::Service::Error::ClientError.new(action, self.name.split('::').first, params, exception)
       end
--- lib/service/error.rb
+++ lib/service/error.rb
@@ -5,7 +5,14 @@ module Service
       def initialize(action, klass, params, response)
         # poor mans underscore
         klass = klass.gsub(/(.)([A-Z])/,'\1_\2').downcase
-        super I18n.t "services.#{klass}.network.#{action.to_s}", { response: response }
+        
+        # error parsing should be api specific.
+        # There are various ways to implement this
+        # any of which could include an error representer
+        #
+        message = response["message"] ? response["message"] : response
+
+        super I18n.t "services.#{klass}.network.#{action.to_s}", { message: message }
       end
     end
 
